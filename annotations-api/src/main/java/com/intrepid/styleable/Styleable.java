c934ce07e09d70e8bce7a02d97d82e1c094c1113
weathersboyle <rob@intrepid.io>
2016-03-03 14:21:43 -0500

    created annotation interface

---
+++ annotations-api/src/main/java/com/intrepid/styleable/Styleable.java
@@ -0,0 +1,12 @@
+package com.intrepid.styleable;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Styleable {
+    String defaultVal();
+}
