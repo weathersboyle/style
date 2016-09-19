8b64c3f881719fc40f1e516a919e1c8e1cb57138
weathersboyle <rob@intrepid.io>
2016-09-19 15:44:33 -0400

    support binding invocation via style lib method

--- app/src/main/java/com/intrepid/style/ui/DemoStyleableView.java
+++ app/src/main/java/com/intrepid/style/ui/DemoStyleableView.java
@@ -10,22 +10,32 @@ import style.Styleable;
 
 public class DemoStyleableView extends FrameLayout {
     @Styleable(attrType = AttributeType.DIMENSION)
-    public float field1;
+    public float width;
 
     @Styleable(attrType = AttributeType.BOOLEAN, defValueBoolean = true)
-    public boolean field2;
+    public boolean fillIn;
 
     @Styleable(attrType = AttributeType.DRAWABLE)
-    public Drawable field3;
+    public Drawable backgroundDrawable;
 
     @Styleable(attrType = AttributeType.INT, defValueInt = 5)
-    public Integer field4;
+    public Integer quantity;
 
     public DemoStyleableView(Context context) {
         super(context);
+        init();
     }
 
     public DemoStyleableView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initWithAttrs(attrs);
+    }
+
+    private void initWithAttrs(AttributeSet attrs) {
+        init();
+    }
+
+    private void init() {
+
     }
 }
